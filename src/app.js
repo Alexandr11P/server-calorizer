8a703c439dcbb6af9b3483c382c69a839eae38f0
Alexandr11P <alexlevelup888@gmail.com>
2024-05-20 22:05:49 +0700

    folder static public_html

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(express.static('./src/static'));
+app.use(express.static('./public_html'));
 app.use('/', router)
 
 async function startApp() {
