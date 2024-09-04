b2e450c7eec9d4b8adc2d86fb540a1883c0b5ffc
Alexandr11P <alexlevelup888@gmail.com>
2024-09-04 17:41:18 +0700

    fixed react router

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import router from './router.js'
-import cors from 'cors'
+import path from 'path'
 
-const PORT = 80;
+const PORT = 5555;
 
 const app = express();
 
 
 app.use(express.json());
-app.use(express.static('./public_html'));
-app.use('/', router)
+app.use(express.static('./static'));
+app.use('/api', router)
+app.use('*', (req, res) => {
+    res.sendFile(`${path.resolve()}/static/index.html`);
+});
 
 async function startApp() {
     try {
